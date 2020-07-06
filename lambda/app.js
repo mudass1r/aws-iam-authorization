393745461d3fd8ac8afdd86a359b03996386f528
mudass1r <57070382+mudass1r@users.noreply.github.com>
2020-07-06 10:55:09 +0530

    Update app.js

--- lambda/app.js
+++ lambda/app.js
@@ -11,7 +11,7 @@
     app.use(cors())
     
     app.use((req, res, next) => {
-        res.header('Access-Control-Allow-Origin', '*');
+        //res.header('Access-Control-Allow-Origin', '*');
         res.header('Content-Type', 'application/json');
         res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
         res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method,X-Access-Token,XKey,Authorization');
